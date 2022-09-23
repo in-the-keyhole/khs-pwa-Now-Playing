c618a7e8e6839766f8de2df479d1d92da799dbba
Jaime Niswonger <jniswonger@keyholesoftware.com>
2022-09-23 20:35:51 +0000

    added responsive menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,24 +3,40 @@ import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorageState from 'use-local-storage-state'
 
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Header = () => {
 
   const navigate = useNavigate();
   const [loggedIn, setLoggedIn] = useLocalStorageState("loggedIn", { defaultValue: false });
+
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  }
+
   const logout = () => {
+    setMenuActive(false);
     setLoggedIn(false);
     navigate("/");
   }
+
   return (
     <nav>
-      <ul className="menu">
+      <ul className={'menu ' + (menuActive ? 'active' : '')}>
         <li className="logo">
           <Link to="/"><img src={logo} alt="logo" /></Link>
         </li>
-        {/* <li className="item"><Link to="/">NowPlaying</Link></li>  */}
-        {loggedIn && <li className="item"><button onClick={logout}>Logout</button></li>}
-        {/* <li className="toggle"><a href="/"><i className='fas fa-bars'></i></a></li> */}
+        {loggedIn &&
+          <>
+            {/* <li className="item"><Link to="/">NowPlaying</Link></li> */}
+            <li className="item"><span onClick={logout}>Logout</span></li>
+            <li className="toggle"><span onClick={toggleMenu}><FontAwesomeIcon icon={menuActive ? faTimes : faBars} /></span></li>
+          </>
+        }
       </ul>
     </nav>
   );
